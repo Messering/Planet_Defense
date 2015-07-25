b5ee8705b1a92e5744d4184009d217c017ee3714
Messering <messering@yandex.ru>
2015-07-25 15:12:50 +0300

    collider and mass and destroy

--- Proba/Assets/mygame/Scripts/Colli/Coll.cs
+++ Proba/Assets/mygame/Scripts/Colli/Coll.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine; 
 using System.Collections; 
 public class Coll : MonoBehaviour { 
-	void OnCollisionEnter2D(Collision2D other)
+
+	public Rigidbody2D b;
+	public GameObject planet;
+	private float a = 1;
+	void OnTriggerEnter2D(Collider2D other)
 	{
+	
+		Destroy (other.gameObject);
+		a += b.mass / 2;
+		planet.transform.localScale = new Vector2 (a,a);
 
 	}
 }

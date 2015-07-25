6f9470e18c2942378233bc22ae93c085e3b0257f
Messering <messering@yandex.ru>
2015-07-25 16:06:57 +0300

    add scale mass asteroid[]

--- Proba/Assets/mygame/Scripts/Colli/Coll.cs
+++ Proba/Assets/mygame/Scripts/Colli/Coll.cs
@@ -3,13 +3,30 @@ using System.Collections;
 public class Coll : MonoBehaviour { 
 
 	public Rigidbody2D b;
+	public Rigidbody2D d;
+	public Rigidbody2D c;
 	public GameObject planet;
 	private float a = 1;
 	void OnTriggerEnter2D(Collider2D other)
 	{
 	
 		Destroy (other.gameObject);
-		a += b.mass / 2;
+
+		if (other.name == "Bird(Clone)") {
+			a += b.mass / 2;
+			print (a);
+		}
+
+		if( other.name =="Bird1(Clone)"){
+				a += d.mass/2;
+				print (a);
+			}
+		if( other.name =="Bird2(Clone)"){
+				a += c.mass/2;
+				print (a);
+			}
+
+
 		planet.transform.localScale = new Vector2 (a,a);
 
 	}
